6453190ffc23c2f23216d3bf1617fa81b79999df
legendecas <legendecas@gmail.com>
2022-01-20 14:28:06 +0800

    extract crashing piece

--- build-snapshot.js
+++ build-snapshot.js
@@ -1,3 +1,20 @@
 'use strict';
 
-globalThis.sdk = require('aliyun-sdk');
+// extracted from https://github.com/isaacs/sax-js/blob/v0.6.1/lib/sax.js#L255
+
+// character classes and tokens
+var number = "0124356789"
+  , letter = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  // (Letter | "_" | ":")
+  , entity = number+letter+"#"
+
+// turn all the string character sets into character class objects.
+
+entity = charClass(entity)
+
+function charClass (str) {
+  return str.split("").reduce(function (s, c) {
+    s[c] = true
+    return s
+  }, {})
+}
